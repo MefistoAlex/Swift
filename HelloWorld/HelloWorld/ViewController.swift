c57541773795dcbce6e256c1ef84783abfb9ff4c
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-07-14 12:21:32 +0300

    ended example from Lesson 1  Block 2

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var helloWorldLabel: UILabel!
 
+    @IBOutlet var showTextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWorldLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func makeButtonActoin() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            showTextButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            showTextButton.setTitle("Show text", for: .normal)
+        }
+       
+    }
+    
 }
-
