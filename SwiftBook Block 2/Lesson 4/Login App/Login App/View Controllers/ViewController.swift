58b64cf4f9dda4801628d6de8012d1aff9053fdc
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-07-26 17:42:55 +0300

    first screen is done

--- SwiftBook Block 2/Lesson 4/Login App/Login App/View Controllers/ViewController.swift
+++ SwiftBook Block 2/Lesson 4/Login App/Login App/View Controllers/ViewController.swift
@@ -5,19 +5,43 @@
 //  Created by Alexandr Mefisto on 26.07.2022.
 //
 
+import SwiftUI
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Privates
+    private var alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
+    
+    @IBOutlet private weak var userName: UITextField!
+    @IBOutlet private weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
         // Do any additional setup after loading the view.
     }
-
+    
+    //MARK: Actions
+    
     @IBAction private func forgetPassword() {
+        alert.title = "You forget password!"
+        alert.message = "Mefisto"
+        present(alert, animated: true)
     }
+
     @IBAction private func forgetUserName() {
+        alert.title = "You forget user name!"
+        alert.message = "Alex"
+        present(alert, animated: true)
     }
     
+    @IBAction private func logIn() {
+        guard userName.text == "Alex" && password.text == "Mefisto" else {
+            alert.title = "Incorrect Username or Password"
+            alert.message = "Press \"Forgot password\" to help"
+            present(alert, animated: true)
+            return
+        }
+        performSegue(withIdentifier: "logined", sender: self)
+    }
 }
-
